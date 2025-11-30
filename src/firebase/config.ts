f6f29a12c25956e5008aa562811cf45c55aea4a0
rs691 <robstewart@my365.bellevue.edu>
2025-11-29 23:02:00 -0600

    added new files 3

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
-// export const firebaseConfig = {
-//   "projectId": "studio-2289885776-3fc9e",
-//   "appId": "1:378041796750:web:77c6dfc5ff2f076084e1df",
-//   "apiKey": "AIzaSyB_DPW2GpzcnkpdhL5PqDTSOPj5px8Rwf0",
-//   "authDomain": "studio-2289885776-3fc9e.firebaseapp.com",
-//   "measurementId": "",
-//   "messagingSenderId": "378041796750"
-// };
+export const firebaseConfig = {
+  "projectId": "studio-2289885776-3fc9e",
+  "appId": "1:378041796750:web:77c6dfc5ff2f076084e1df",
+  "apiKey": "AIzaSyB_DPW2GpzcnkpdhL5PqDTSOPj5px8Rwf0",
+  "authDomain": "studio-2289885776-3fc9e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "378041796750"
+};
